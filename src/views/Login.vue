3d583ee88fdef7b387fe61a0bd2d78aaa9e41ebc
LuizGenuino <luizgustavo@pitom.com.br>
2023-11-17 14:10:18 -0400

    feat: integrates login screen with API

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,38 @@
+
 <template>
-  <v-container class="fill-height">
-    <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
-        <v-card class="elevation-12">
-          <v-card-title>
-            <img src="@/assets/logo.png" alt="Logo" class="logo" />
-            Login
-          </v-card-title>
-          <v-card-text>
-            <v-form @submit.prevent="login">
-              <v-text-field
-                v-model="email"
-                label="Email"
-                required
-                :error-messages="emailErrors"
-                @input="validateEmail"
-              ></v-text-field>
-              <v-text-field
-                v-model="password"
-                label="Senha"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                :type="showPassword ? 'text' : 'password'"
-                @click:append="showPassword = !showPassword"
-                required
-              ></v-text-field>
-              <v-btn type="submit" color="primary"> Entrar </v-btn>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4" class="m-400">
+          <v-card class="elevation-12 pa-2">
+            <v-card-title class="mb-4 d-flex justify-center align-center" >
+              <img src="@/assets/logo.png" alt="Logo" class="logo" />
+            </v-card-title>
+            <v-card-text>
+              <v-form @submit.prevent="login">
+                <v-text-field
+                  v-model="email"
+                  label="Email"
+                  required
+                  :error-messages="emailErrors"
+                  @input="validateEmail"
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  label="Senha"
+                  :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                  :type="showPassword ? 'text' : 'password'"
+                  @click:append="showPassword = !showPassword"
+                  required
+                ></v-text-field>
+                <v-btn type="submit" color="primary" class="mt-4" > Entrar </v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
 </template>
 
 <script>
-import { AuthService } from '@/integrations/services';
+import { AuthService } from "@/integrations/services";
 export default {
   // eslint-disable-next-line
   name: "Login",
@@ -56,16 +54,21 @@ export default {
       }
     },
     async login() {
-      try {
-        const authService = new AuthService()
-        const {data} = await authService.insert({email: this.email, password:this.password})
-        console.log(data);
-        localStorage.setItem("ToDoList@token", data.token)
-        localStorage.setItem("ToDoList@user", JSON.stringify(data.user))
-        this.$router.push('/tarefas')
+      if(this.email.length > 4 && this.password.length > 4){
+        try {
+        const authService = new AuthService();
+        const { data } = await authService.insert({
+          email: this.email,
+          password: this.password,
+        });
+        localStorage.setItem("ToDoList@token", data.token);
+        localStorage.setItem("ToDoList@user", JSON.stringify(data.user));
+        this.$router.push("/tarefas");
       } catch (err) {
-        console.error(err)
-        this.$alertaGlobal('error', err.message);
+        this.$getAlertaGlobal().exibirAlerta("error", err.message);
+      }
+      }else{
+        this.$getAlertaGlobal().exibirAlerta("error", "Preencha os Campos corretamente!");
       }
     },
   },
@@ -74,7 +77,13 @@ export default {
 
 <style scoped>
 .logo {
-  width: 40px;
-  margin-right: 8px;
+  width: 150px;
+}
+
+.content {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
